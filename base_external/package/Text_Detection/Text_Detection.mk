cdb7f6f6f2655006f873fc37c525cde0f97ba9a6
Sriramz2002 <sriramkumar.jayaraman@colorado.edu>
2025-04-30 11:11:00 -0600

    Buildroot upDATE

--- base_external/package/Text_Detection/Text_Detection.mk
+++ base_external/package/Text_Detection/Text_Detection.mk
@@ -4,36 +4,28 @@
 #
 ##############################################################
 
-# Reference your Assignment 3 repository
-TEXT_DETECTION_VERSION =  ad1abbbee22fb3e52fb158eba7fcbb3a92956d16 
+# Reference your Final Repo
+TEXT_DETECTION_VERSION =  04f72f458b280ee242e221043a02e82a963a095c
 
 # Use SSH for git access
-TEXT_DETECTION_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Sriramz2002.git
+TEXT_DETECTION_SITE = git@github.com:cu-ecen-aeld/final-project-Sriramz2002.git
 TEXT_DETECTION_SITE_METHOD = git
 TEXT_DETECTION_GIT_SUBMODULES = YES
 
 
 define TEXT_DETECTION_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-
-
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/v4l2_dev_driver
 endef
 
 
 
 define TEXT_DETECTION_INSTALL_TARGET_CMDS
 
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	# Install C binary
+	$(INSTALL) -m 0755 $(@D)/v4l2_dev_driver/simple_capture $(TARGET_DIR)/usr/bin
 
-
-	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-
-
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	# Install Python script
+	$(INSTALL) -m 0755 $(@D)/v4l2_dev_driver/sender.py $(TARGET_DIR)/usr/bin
 
 
 endef
